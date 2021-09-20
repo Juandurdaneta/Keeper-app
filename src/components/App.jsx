813cd50bbbec0a744460ddff654650e9c6384d5f
Juan Urdaneta <juandurdanet@gmail.com>
2021-09-20 10:38:23 -0400

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+
+function App() {
+    return (
+        <div>
+        <Header/>
+        {notes.map(entry =>
+            <Note title={entry.title} content={entry.content}/>
+        )}
+        <Footer />
+        </div>
+    )
+}
+
+export default App;
