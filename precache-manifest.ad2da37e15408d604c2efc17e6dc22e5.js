cc5710614692100a2c04132073e36ee9eaafd345
Juan Urdaneta <juandurdanet@gmail.com>
2021-09-24 14:12:49 -0400

    Updates

---
+++ precache-manifest.ad2da37e15408d604c2efc17e6dc22e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909829df8a0bdefcb73258add670bcaf",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "abc719034a6d32a84136",
+    "url": "/Keeper-app/static/js/2.7c83e95b.chunk.js"
+  },
+  {
+    "revision": "fdc37bec395ca311b8d0",
+    "url": "/Keeper-app/static/js/main.311b2ed0.chunk.js"
+  },
+  {
+    "revision": "33cb2174e8328f4b935a",
+    "url": "/Keeper-app/static/js/runtime-main.b2f67e31.js"
+  }
+]);
